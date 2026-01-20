831f4a044832a750ce2e8c73dd2db0c116abf5cd
Ubuntu <naturaltwenty@NaturalTwenty.neo.internal>
2026-01-20 23:03:31 +0700

    fix reverb!

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class AdminPanelProvider extends PanelProvider
@@ -54,6 +55,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+	    ])
+	    ->renderHook(
+        	'panels::body.end',
+        	fn (): string => Blade::render("@vite('resources/js/app.js')")
+    	    );
     }
 }
